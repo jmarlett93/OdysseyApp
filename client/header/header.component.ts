3782e7fd18e6d5fa4f55cf18fe37640415ba86ed
jmarlett93 <1fiyero@gmail.com>
2017-05-28 22:05:25 -0500

    fixed package compiling

--- client/header/header.component.ts
+++ client/header/header.component.ts
@@ -3,5 +3,6 @@ import {Component} from "@angular/core";
 @Component({
     selector:'base-app',
     template: require('./header.component.html'),
+    styles: [require('styles.less')]
 })
 export class HeaderComponent{}
--- webpack.config.js
+++ webpack.config.js
@@ -11,22 +11,13 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.ts$/, loader: 'ts'},
-      {test: /\.html$/, loader: 'raw'}
+      {test: /\.html$/, loader: 'raw'},
+      {test: /\.less$/,
+       exclude: /node_modules/,
+       loader: 'raw-loader!less-loader' }
     ]
-    },
-
-     module: {
-        rules: [{
-            test: /\.less$/,
-            use: [{
-                loader: "style-loader" // creates style nodes from JS strings
-            }, {
-                loader: "css-loader" // translates CSS into CommonJS
-            }, {
-                loader: "less-loader" // compiles Less to CSS
-            }]
-        }]
-    },
+  },
+  
   resolve: {
     extensions: ['', '.js', '.ts', '.html', '.css']
   },
